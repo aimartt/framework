badad943575642693ec5a7a1e9aa335ae3108a53
aimartt <aimartt@gmail.com>
2015-09-17 11:58:34 +0800

    异常包

---
+++ src/main/java/com/aimartt/framework/SearchFilter.java
@@ -0,0 +1,142 @@
+package com.aimartt.framework;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang3.StringUtils;
+
+import com.aimartt.framework.exception.BusinessException;
+import com.google.common.collect.Lists;
+
+/**
+ * Title:
+ * <p></p>
+ * Copyright (c) aimartt
+ * @author aimartt on 2015年9月17日
+ */
+public class SearchFilter {
+
+	public final String fieldName;
+	public final String originalFieldName;
+	public Object value;
+	public final Object originalValue;
+	public final Operator operator;
+	
+	/**
+	 * <p>覆盖字段名称，将驼峰式的字符串替换为下划线分隔的字符串。</p>
+	 * <p>如：userName 将返回 user_name</p>
+	 * @param columnName 字段名称
+	 * @return
+	 * @author aimartt on 2015年9月17日
+	 */
+	public static String coverdColumnName(String columnName) {
+		StringBuffer str = new StringBuffer();
+		for (int i = 0; i < columnName.length(); i++) {
+			char c = columnName.charAt(i);
+			if (!Character.isLowerCase(c) && c != 46 && c != 95) {
+				str.append((char) 95).append((char) (c + 32));
+			} else {
+				str.append(c);
+			}
+		}
+		return str.toString();
+	}
+
+	/**
+	 * <p>解析查询条件</p>
+	 * @param searchParams
+	 * @return
+	 * @author aimartt on 2015年9月17日
+	 */
+	public static List<SearchFilter> parse(Map<String, Object> searchParams) {
+		List<SearchFilter> filters = Lists.newArrayList();
+		if (searchParams == null) {
+			return Collections.emptyList();
+		}
+		for (Entry<String, Object> entry : searchParams.entrySet()) {
+			String key = entry.getKey();
+			if (!StringUtils.contains(key, '_')) {
+				throw new BusinessException("查询条件不符合规范!");
+			}
+			int index = StringUtils.indexOfAny(key, "_");
+			String[] names = StringUtils.split(key, "_", (index + 1));
+			if (names.length < 2) {
+				throw new BusinessException("查询条件截取出错!");
+			}
+			Operator op = Operator.getOpeartor(names[0]);
+			if (op == null) {
+				throw new BusinessException("查询方式未定义!");
+			}
+			String filedName = names[1];
+			Object value = entry.getValue();
+			if (value == null || StringUtils.isBlank(String.valueOf(value))) {
+				continue;
+			}
+			filters.add(new SearchFilter(filedName, coverdColumnName(filedName), op, value));
+		}
+		return filters;
+	}
+
+	public SearchFilter(String fieldName, String originalFieldName, Operator operator, Object value) {
+		this.fieldName = fieldName;
+		this.originalFieldName = originalFieldName;
+		this.value = value;
+		this.originalValue = value;
+		this.operator = operator;
+	}
+
+	/**
+	 * <p>条件操作符枚举</p>
+	 * Copyright (c) aimartt
+	 * @author aimartt on 2015年9月17日
+	 */
+	public enum Operator {
+		/** Equal */
+		EQ,
+		/** Not equal */
+		NOTEQ,
+		/** Like */
+		LIKE,
+		/** Left like */
+		LLIKE,
+		/** Right like */
+		RLIKE,
+		/** Not like */
+		NLIKE,
+		/** Great than */
+		GT,
+		/** Less than */
+		LT,
+		/** Great than or equal */
+		GTE,
+		/** Less than or equal */
+		LTE,
+		/** In */
+		IN,
+		/** Not in */
+		NOTIN,
+		/** Is null */
+		NULL,
+		/** Is not null */
+		NOTNULL;
+	
+		public static Operator getOpeartor(String name) {
+			for (Operator op : Operator.values()) {
+				if (op.toString().equals(name)) {
+					return op;
+				}
+			}
+			return null;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+	
+}
---
+++ src/main/java/com/aimartt/framework/exception/AbstractI18NMessageException.java
@@ -0,0 +1,55 @@
+package com.aimartt.framework.exception;
+
+/**
+ * 异常抽象类,将错误信息国际化
+ * @author pu.zhang
+ */
+public abstract class AbstractI18NMessageException extends RuntimeException {
+	
+	private static final long serialVersionUID = 2148374270769534530L;
+
+	static final int DEFAULT_ERROR_CODE = 0;
+
+	/** 错误代码,默认为未知错误 */
+	private int errorCode;
+	/** 调试信息 */
+	protected String errorMessage;
+
+	public AbstractI18NMessageException(String errorMessage) {
+		this(errorMessage, null);
+	}
+
+	public AbstractI18NMessageException(int errorCode, String errorMessage) {
+		this(errorCode, errorMessage, null);
+	}
+
+	public AbstractI18NMessageException(String errorMessage, Throwable cause) {
+		this(DEFAULT_ERROR_CODE, errorMessage, cause);
+	}
+
+	public AbstractI18NMessageException(int errorCode, Throwable cause) {
+		this(errorCode, null, cause);
+	}
+
+	public AbstractI18NMessageException(int errorCode, String errorMessage, Throwable cause) {
+		super(errorMessage, cause);
+		this.errorCode = errorCode;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+}
